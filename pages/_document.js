29e6bcf1981c2de31464cb9052602b621ac70a69
Abe Rubenstein <abe.rubenstein@datadoghq.com>
2021-03-06 15:34:12 -0500

    update icon

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ class MyDocument extends Document {
           />
 
           <link rel="icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" href="/kitsuneflower.png" />
+          <link rel="apple-touch-icon" href="/kitsunelogo.png" />
         </Head>
         <body>
           <Main />
