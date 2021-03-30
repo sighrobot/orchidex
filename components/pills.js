3b799b5c8befb015646fa6c68aca44b7add366ef
Abe Rubenstein <abe.rubenstein@datadoghq.com>
2021-03-29 21:19:35 -0400

    tighten pill logic

--- components/pills.js
+++ components/pills.js
@@ -1,20 +1,33 @@
 import { Pill } from "components/pill";
-import { CROSS_CHAR } from "lib/string";
+import { CROSS_CHAR, UNKNOWN_CHAR } from "lib/string";
 
 export const Pills = ({ grex }) => {
   const pills = [];
 
+  const hasParents = grex.seed_parent_epithet && grex.pollen_parent_epithet;
+
+  const isParentNaturalHybrid =
+    grex.seed_parent_epithet[0] === UNKNOWN_CHAR ||
+    grex.pollen_parent_epithet[0] === UNKNOWN_CHAR;
+
   const isIntergeneric = grex.seed_parent_genus !== grex.pollen_parent_genus;
+
   const isNaturalHybrid =
     grex.epithet[0] === CROSS_CHAR ||
-    (grex.seed_parent_epithet &&
-      grex.pollen_parent_epithet &&
-      grex.registrant_name === "This is a natural hybrid");
+    (hasParents && grex.registrant_name === "This is a natural hybrid") ||
+    (hasParents &&
+      !isParentNaturalHybrid &&
+      grex.epithet &&
+      grex.epithet[0] == grex.epithet[0].toLowerCase());
+
   const isSpecies =
+    !isParentNaturalHybrid &&
     !isNaturalHybrid &&
     grex.epithet &&
     grex.epithet[0] === grex.epithet[0].toLowerCase();
+
   const isPrimaryHybrid =
+    !isParentNaturalHybrid &&
     !isNaturalHybrid &&
     grex.seed_parent_epithet &&
     grex.seed_parent_epithet[0] === grex.seed_parent_epithet[0].toLowerCase() &&
@@ -26,16 +39,16 @@ export const Pills = ({ grex }) => {
     pills.push("species");
   }
 
+  if (isNaturalHybrid) {
+    pills.push("natural");
+  }
+
   if (isIntergeneric) {
     pills.push("intergeneric");
   }
 
   if (isPrimaryHybrid) {
     pills.push("primary");
-  }
-
-  if (isNaturalHybrid) {
-    pills.push("natural");
   }
 
   if (pills.length === 0) {
