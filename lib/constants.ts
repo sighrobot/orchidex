61534b85c81fc757424f213272575532d8c296e5
Abe Rubenstein <sighrobot@gmail.com>
2023-02-04 17:31:49 -0500

    fix deploy url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,4 @@
-const DEFAULT_APP_URL = `https://${
-  process.env.NEXT_PUBLIC_VERCEL_URL || 'grex.es'
-}`;
+const DEFAULT_APP_URL = `https://${process.env.VERCEL_URL || 'grex.es'}`;
 export const APP_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
