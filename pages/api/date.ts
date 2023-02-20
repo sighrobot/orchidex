39108dfa22b93c4edd94f7ec32259b7ce3bbed04
Abe Rubenstein <sighrobot@gmail.com>
2023-02-20 13:56:50 -0500

    date: don't fetch empty epithets

--- pages/api/date.ts
+++ pages/api/date.ts
@@ -11,12 +11,12 @@ export default async (req: NextRequest) => {
   let expr = '';
 
   if (date) {
-    expr += `SELECT * FROM rhs WHERE date_of_registration = '${date}'`;
+    expr += `SELECT * FROM rhs WHERE epithet != '' AND date_of_registration = '${date}'`;
   } else {
     const d = new Date();
     d.setDate(d.getDate() - 9);
 
-    expr += `SELECT * FROM rhs WHERE date(date_of_registration) >= '${d
+    expr += `SELECT * FROM rhs WHERE epithet != '' AND date(date_of_registration) >= '${d
       .toISOString()
       .slice(0, 10)}' ORDER BY date_of_registration DESC`;
   }
