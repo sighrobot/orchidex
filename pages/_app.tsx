85f423b3a620e7e11daa1d49e6a81c5c4ec1abae
Abe Rubenstein <sighrobot@gmail.com>
2023-03-27 00:03:59 -0400

    add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
 import '../styles/globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+
+      <Script
+        async
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA}`}
+      />
+      <Script
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', '${process.env.GA}');`,
+        }}
+      />
     </>
   );
 }
