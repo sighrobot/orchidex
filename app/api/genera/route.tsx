fac9a142f89397912e1e5544b973d27d297ed9a3
Abe Rubenstein <sighrobot@gmail.com>
2023-08-01 22:10:05 -0400

    query only genus

--- app/api/genera/route.tsx
+++ app/api/genera/route.tsx
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET(req) {
   const data = await query(
-    "SELECT genus g, count(*) c, max(date_of_registration) d, epithet e, id FROM rhs WHERE genus != 'na' AND epithet != '' GROUP BY genus",
+    "SELECT genus g FROM rhs WHERE genus != 'na' AND epithet != '' GROUP BY genus",
   );
   const json = await data?.json();
 
