e4f69ab51599e21b817c66b5b96f20ab54022ff3
Abe Rubenstein <abe.rubenstein@datadoghq.com>
2023-09-24 12:05:45 -0400

    add full-text search route and fetchers

---
+++ lib/fetchers/fts.ts
@@ -0,0 +1,35 @@
+import { Grex } from 'lib/types';
+import React from 'react';
+
+type FetchFTSProps = { q: string; offset?: number };
+
+function makeUrl({ q, offset }: FetchFTSProps = { q: '' }) {
+  let url = `/api/fts/${encodeURIComponent(q)}`;
+
+  if (offset) {
+    url += `?offset=${offset}`;
+  }
+  return url;
+}
+
+export async function fetchFTS({ q, offset }: FetchFTSProps = { q: '' }) {
+  const url = makeUrl({ q, offset });
+  const fetched = await fetch(url);
+  return fetched.json();
+}
+
+export function useFTS({ q, offset }: FetchFTSProps) {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [data, setData] = React.useState<Grex[]>([]);
+
+  React.useEffect(() => {
+    if (!q) return;
+    setIsLoading(true);
+    fetchFTS({ q, offset }).then((json) => {
+      setData(json);
+      setIsLoading(false);
+    });
+  }, [q, offset]);
+
+  return { isLoading, data };
+}
