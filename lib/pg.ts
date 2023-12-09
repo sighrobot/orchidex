c450e80837a98705c68b15831e1922a9a06b4d3c
Abe Rubenstein <sighrobot@gmail.com>
2023-12-09 11:55:58 -0500

    remove vercel client

--- lib/pg.ts
+++
@@ -1,9 +0,0 @@
-import { sql } from '@vercel/postgres';
-
-export const query = async (expr: string) => {
-  if (!expr) return null;
-
-  const { rows } = await sql.query(expr);
-
-  return rows;
-};
