cbafd9e6888607024bb7a238d1b41612556fec36
Abe Rubenstein <abe.rubenstein@datadoghq.com>
2024-02-14 16:46:47 -0500

    add dd rum

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,6 +45,32 @@ export default function RootLayout({
           </>
         )}
 
+        <Script id='datadog-rum'>
+          {`
+             (function(h,o,u,n,d) {
+               h=h[d]=h[d]||{q:[],onReady:function(c){h.q.push(c)}}
+               d=o.createElement(u);d.async=1;d.src=n
+               n=o.getElementsByTagName(u)[0];n.parentNode.insertBefore(d,n)
+             })(window,document,'script','https://www.datadoghq-browser-agent.com/us1/v5/datadog-rum.js','DD_RUM')
+             window.DD_RUM.onReady(function() {
+               window.DD_RUM.init({
+                 clientToken: '${process.env.DD_RUM_CLIENT_TOKEN}',
+                 applicationId: '${process.env.DD_RUM_APP_ID}',
+                 site: 'datadoghq.com',
+                 service: 'next-app-router-rum',
+                 env: 'prod',
+                 version: '${process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 7)}',
+                 sessionSampleRate: 100,
+                 sessionReplaySampleRate: 100,
+                 trackUserInteractions: true,
+                 trackResources: true,
+                 trackLongTasks: true,
+                 defaultPrivacyLevel: 'allow',
+               });
+             })
+           `}
+        </Script>
+
         <div className={style.container}>
           {children}
           <Disclaimer />
