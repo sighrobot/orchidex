9abf7402a4ec0c699dab1c86117457e8d9470a11
Abe Rubenstein <sighrobot@gmail.com>
2024-07-28 11:53:58 -0400

    bump pool size

--- lib/storage/pool.ts
+++ lib/storage/pool.ts
@@ -1,7 +1,10 @@
 import { Pool } from 'pg';
 
 if (!global.pool) {
-  global.pool = new Pool({ connectionString: process.env.SB_PG_POOL_URL });
+  global.pool = new Pool({
+    connectionString: process.env.SB_PG_POOL_URL,
+    max: 100,
+  });
 }
 
 export default global.pool;
