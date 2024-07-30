8d4700c907e1f46babc2d01adb473ba2bc88f037
Abe Rubenstein <sighrobot@gmail.com>
2024-07-29 21:55:11 -0400

    try releasing pool client

--- lib/storage/pool.ts
+++ lib/storage/pool.ts
@@ -1,10 +1,20 @@
 import { Pool } from 'pg';
 
+const pool = new Pool({ connectionString: process.env.SB_PG_POOL_URL });
+
 if (!global.pool) {
-  global.pool = new Pool({
-    connectionString: process.env.SB_PG_POOL_URL,
-    max: 100,
-  });
+  global.pool = {
+    query: async (sql: string) => {
+      const client = await pool.connect();
+      let response;
+      try {
+        response = await client.query(sql);
+      } finally {
+        client.release();
+      }
+      return response;
+    },
+  };
 }
 
 export default global.pool;
