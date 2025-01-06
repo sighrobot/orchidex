36aec9aea0ee9ba5b48e2c1ef8fb9bed76f5ba04
Abe Rubenstein <sighrobot@gmail.com>
2025-01-05 20:56:58 -0500

    improve date presentation

--- components/grex/date/index.tsx
+++ components/grex/date/index.tsx
@@ -1,9 +1,18 @@
 import { Grex } from 'lib/types';
 
-export const RegDate = ({ grex }: { grex: Grex }) => {
-  const dateStr = new Date(`${grex.date_of_registration}T00:00:00`)
+export const RegDate = ({
+  className,
+  grex,
+}: {
+  className?: string;
+  grex: Grex;
+}) => {
+  const parts = new Date(`${grex.date_of_registration}T00:00:00`)
     .toString()
-    .slice(4, 15);
+    .slice(4, 15)
+    .split(' ');
 
-  return dateStr;
+  const dateStr = `${parts[0]} ${parseInt(parts[1], 10)} ${parts[2]}`;
+
+  return <span className={className}>{dateStr}</span>;
 };
